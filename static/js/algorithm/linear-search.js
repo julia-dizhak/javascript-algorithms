cdcf0a3a84df49543ede743699bb583f9d96b19a
Yulia <yulia.scherbina@gmail.com>
2017-11-05 12:23:43 +0100

    linear search(indexof), sum of two cubes

---
+++ static/js/algorithm/linear-search.js
@@ -0,0 +1,44 @@
+/**
+ * Linear Search ( Array A, Value x)
+ *
+ * Algorithm
+ *
+ * Step 1: Set i to 1
+ * Step 2: if i > n then go to step 7
+ * Step 3: if A[i] = x then go to step 6
+ * Step 4: Set i to i + 1
+ * Step 5: Go to Step 2
+ * Step 6: Print Element x Found at index i and go to step 8
+ * Step 7: Print element not found
+ * Step 8: Exit
+ *
+ * Characteristics
+ *
+ * The worst case performance scenario for a linear search is that it needs to loop through the entire collection;
+ * either because the item is the last one, or because the item isn't found. In other words, if you have N items in your collection, the worst case scenario to find an item is N iterations.
+ * This is known as runtime O(n) using the Big O Notation.
+ * The speed of search grows linearly with the number of items within your collection.
+ *
+ * In The Real World
+ * Find Index
+ *
+ */
+
+/**
+ * @param {array, number}
+ * @return {string[]}
+ */
+
+'use strict';
+
+function findIndex(data, searchQuery) {
+    for (var i = 0; i < data.length; i++) {
+        if ( data[i] === searchQuery) {
+            return data[i];
+        }
+    }
+    return false;
+}
+
+findIndex([1,2,3,4,6], 6);
+//console.log( findIndex([1,2,3,4,6], 6) );
---
+++ static/js/algorithm/sum-of-two-cubes.js
@@ -0,0 +1,121 @@
+/**
+ * Print all positive solutions to the equation a^3+b^3 = c^3 + d^3, where a, b, c, d lie between [0, 10^5].
+ *
+ * Algorithm
+ * ...
+ *
+ * Characteristics
+ *
+ * runtime O(n:^4)
+ * runtime O(n:^3)
+ *
+ * runtime O(n:^2)
+ *
+ */
+
+/**
+ * @param {number}
+ * @return {number a,b,c,d; pair (a,b) (c,d)}
+ *
+ */
+
+ 'use strict';
+
+// runtime O(n:^4)
+// n = 1000
+function defineSumOfTwoCubesRuntimeFour(n) {
+    var counter = 0;
+
+    for (var a = 1; a < n; a++) {
+        for (var b = 1; b < n; b++) {
+            for (var c = 1; c < n; c++) {
+                for (var d = 1; d < n; d++) {
+                    counter += 1;
+                    if (Math.pow(a, 3) + Math.pow(b, 3) == Math.pow(c, 3) + Math.pow(d, 3)) {
+                        console.log(`${counter} attempt`);
+                        console.log(a, b, c, d);
+                        break; // it's unnecessary to continue checking for other possible values of d
+                    }
+                }
+            }
+        }
+    }
+}
+// defineSumOfTwoCubesRuntimeFour(10);
+
+
+// runtime O(n:^3)
+function defineSumOfTwoCubesRuntimeThree(n) {
+    var counter = 0;
+
+    for (var a = 1; a < n; a++) {
+        for (var b = 1; b < n; b++) {
+            for (var c = 1; c < n; c++) {
+                var d = Math.pow(Math.pow(a, 3) + Math.pow(b, 3) - Math.pow(c, 3), 1 / 3);
+                counter += 1;
+                if (Math.pow(a, 3) + Math.pow(b, 3) == Math.pow(c, 3) + Math.pow(d, 3) && d >= 0 && d <= n) { // validate that value works
+                    console.log(`${counter} attempt`);
+                    console.log(a, b, c, d);
+                }
+            }
+        }
+    }
+}
+// defineSumOfTwoCubesRuntimeThree(10);
+
+// runtime O(n:^2)
+
+function defineSumOfTwoCubesRuntimeTwo(n) {
+    let counter = 0;
+    let result = {}, data;
+
+    for (let c = 1; c < n; c++) {
+        for (let d = 1; d < n; d++) {
+            data = Math.pow(c, 3) + Math.pow(d, 3);
+            if (!result[data]) {
+                result[data] = [];
+            }
+            result[data].push([c, d]);
+
+            counter += 1;
+            console.log(`${counter} attempt`);
+        }
+    }
+
+    /** The algorithm operates by essentially iterating through all (a, b) pairs and then searching all (c, d) pairs to find if there are any matches to that (a, b) pairs.
+    * Why do we keep on computing all (c, d) pairs for each (a, b) pair? We should just create a list of (c, d) pairs once.
+    * Then, we have an (a, b) pair, find the matches within the (c, d) list. We can quickly locate the matches by inserting each (c, d) pair into a hash table that maps from the sum
+    * to the pair (or, rather, the list of pairs that have that sum).
+    */
+    // for (var a = 1; a < n; a++) {
+    //     for (var b = 1; b < n; b++) {
+    //         data = Math.pow(a, 3) + Math.pow(b, 3);
+    //
+    //         var solutionsList = result[data];
+    //         console.log(`Soluation a: ${a} b: ${b}`);
+    //         console.log(` paris c, d: ${solutionsList}`)
+    //
+    //
+    //     }
+    // }
+
+    /** Actually, once we have the map of all the (c, d) pairs, we can just use that directly.
+     *  We don't need to generate the (a, b) pairs. Each (a, b) will already be in the map.
+     */
+    for (var key in result) {
+        var solutions = result[key];
+        for (var i=0; i<solutions.length; i++) {
+            var pair = solutions[i];
+
+
+            for (var j=0; j<solutions.length; j++) {
+                var pair2 = solutions[j];
+                console.log(pair, pair2);
+            }
+        }
+    }
+
+    // console.log(result);
+}
+
+defineSumOfTwoCubesRuntimeTwo(10);
