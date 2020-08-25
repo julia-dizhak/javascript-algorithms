788121417e075c26dd094bbfa679685fa094a9d4
Yulia <yulia.scherbina@gmail.com>
2020-08-25 11:42:25 +0200

    JS: implement map method via reduce

---
+++ src/codewars/arrays/implement-map-via-reduce.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5];
+const mappingFunction = v => v + 1;
+
+const mappedArray = arr.map((v,i) => v + i);
+const mappedArray1 = arr.map(mappingFunction);
+//console.log('mappedArray', mappedArray);
+//console.log('mappedArray', mappedArray1);
+
+const customMapping = (arr, mappingFunction) => {
+  // val - iteration value
+  return arr.reduce(
+    (accumulator, value) => [...accumulator, mappingFunction(value)],
+    []
+  );
+}
+//console.log('customMapping', customMapping(arr, mappingFunction));
+
